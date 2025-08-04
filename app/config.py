6e0ea4d1269a9a24018677c75a9f99bd1da7a4bd
Melih Talha <111735096+mamalito345@users.noreply.github.com>
2025-08-04 17:22:37 +0300

    dwa

--- app/config.py
+++ app/config.py
@@ -7,12 +7,6 @@ class Settings(BaseSettings):
     woo_cus: str
     woo_secret: str
     base_url: str
-    gemini_api_key_1: str
-    gemini_api_key_2: str
-    gemini_api_key_3: str
-    gemini_api_key_4: str
-    gemini_api_key_5: str
-    gemini_api_key_6: str
 
     class Config:
         env_file = ".env"
